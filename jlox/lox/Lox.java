2d53e6ebc0092dc93a751d09e9ac9a5db84439b9
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-08-30 13:44:21 -0500

    Initialize jlox and implement scanner through section 4.5.1
    Program can run and scan & recognize single-character tokens!

---
+++ jlox/lox/Lox.java
@@ -0,0 +1,72 @@
+package lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        // Main program entry point, processing CLI args and delegating to 
+        // different run modes.
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        // Read in a file and run the interpreter.
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        // Run as an interactive prompt (REPL).
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        // Run the interpreter for a given source code string.
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        // Emit an error message to stderr.
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        // Report an error by printing the line and message.
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
---
+++ jlox/lox/Scanner.java
@@ -0,0 +1,76 @@
+package lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static lox.TokenType.*;
+
+class Scanner {
+    // The main scanner/tokenizer, responsible for converting the source
+    // code string into a series of Tokens.
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;   // the first char of the lexeme being scanned
+    private int current = 0; // the current character being considered
+    private int line = 1;    // the current line being scanned
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        // Scan the source code one token at a time, add a special EOF token
+        // at the end of file to denote completion.
+        while (!isAtEnd()) {
+            // We are at the beginning of the next lexeme.
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            default:
+                Lox.error(line, "Unexpected character: '" + c + "'.");
+                break;
+        }
+    }
+
+    private boolean isAtEnd() {
+        // True if we have reached the end of the source.
+        return current >= source.length();
+    }
+
+    private char advance() {
+        // Return character at current location, and advance one character.
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        // Add a token with no literal.
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        // Add a new token.
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
