1a50a3ec86fcbc463eec474c25875d272948b4bd
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-08-30 23:31:32 -0500

    Add -c argument to golox

--- golox/lox.go
+++ golox/lox.go
@@ -8,6 +8,12 @@ import (
 	"strings"
 )
 
+func runString(source string) error {
+	fmt.Println("Running string:", source)
+	run(source)
+	return nil
+}
+
 func runFile(filename string) error {
 	fmt.Println("Running file:", filename)
 	b, err := ioutil.ReadFile(filename)
@@ -46,7 +52,13 @@ func run(source string) {
 
 func runLoxMain() int {
 	// Run the main Lox interpreter, either by passing in a filename or as a REPL
-	if len(os.Args) > 2 {
+	if len(os.Args) == 3 && os.Args[1] == "-c" {
+		err := runString(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			return 1
+		}
+	} else if len(os.Args) > 2 {
 		fmt.Println("Usage: golox [script]")
 		return 64
 	} else if len(os.Args) == 2 {
