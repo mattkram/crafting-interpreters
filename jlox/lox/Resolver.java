3d733d5c49e39c5cd954e885ced8b891b056fe0c
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-09-13 23:34:39 -0500

    Add error handling for improper uses of 'super'

--- jlox/lox/Resolver.java
+++ jlox/lox/Resolver.java
@@ -19,7 +19,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 
     private ClassType currentClass = ClassType.NONE;
@@ -113,6 +114,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
         }
 
@@ -263,6 +265,14 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword,
+                "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword,
+                "Can't use 'super' in a class with no superclass.");
+        }
+
         resolveLocal(expr, expr.keyword);
         return null;
     }
