d720ac63e75c4349be900f26d6117339a9295827
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-09-16 12:51:05 -0500

    Add basic class to represent opcodes and memory management

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#include "common.h"
+#include "chunk.h"
+
 int main(int argc, const char* argv[]) {
-    printf("Hello World!\n");
+    printf("Welcome to clox!\n");
+
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
     return 0;
 }
