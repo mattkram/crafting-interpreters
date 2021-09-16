a7c3b56e69de7d96bae4c885ae239bd313fe6acf
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-09-16 12:57:44 -0500

    Add basic disassembler for debugging

--- clox/src/main.c
+++ clox/src/main.c
@@ -2,6 +2,7 @@
 
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
     printf("Welcome to clox!\n");
@@ -9,6 +10,7 @@ int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 
     return 0;
