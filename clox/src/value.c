b7397ee14ea61e43254da232dd69eab682778587
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-10-03 21:55:29 -0500

    Add string printing and comparison

--- clox/src/value.c
+++ clox/src/value.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -37,6 +39,9 @@ void printValue(Value value) {
         case VAL_NUMBER:
             printf("%g", AS_NUMBER(value));
             break;
+        case VAL_OBJ:
+            printObject(value);
+            break;
     }
 }
 
@@ -46,6 +51,12 @@ bool valuesEqual(Value a, Value b) {
         case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
         case VAL_NIL:    return true;
         case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        case VAL_OBJ: {
+            ObjString* aString = AS_STRING(a);
+            ObjString* bString = AS_STRING(b);
+            return aString->length == bString->length &&
+                memcmp(aString->chars, bString->chars, aString->length) == 0;            
+        }
         default:         return false; // Unreachable.
     }
 }
